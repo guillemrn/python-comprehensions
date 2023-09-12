291d4a2d8f36941441264ca640117acd8edc1aef
Guillermo Moreno <guillermolpzm@gmail.com>
2023-09-11 20:31:08 -0600

    sets

---
+++ 01_set.py
@@ -0,0 +1,22 @@
+set_countries = {'col', 'mex', 'bol'}
+print(set_countries)
+print(type(set_countries))
+
+set_numbers = {1, 2, 3, 2, 4, 5 ,6}
+print(set_numbers)
+
+set_types = {1, True, 'hola', 5.5}
+print(set_types)
+
+set_from_string = set('hola')
+print(set_from_string)
+
+set_from_truple = set(('abc', 'cbv', 'as', 'abc'))
+print(set_from_truple)
+
+numbers = [1,2,3,4,4,2,5,5]
+set_numbers = set(numbers)
+print(set_numbers)
+
+unique_numbers = list(set_numbers)
+print(unique_numbers)
